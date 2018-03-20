8a12484ed9ca3ce1cda1338526fa0eea33b4aa5a
mahendraborana161 <37539391+mahendraborana161@users.noreply.github.com>
2018-03-21 02:10:58 +0530

    Add files via upload

---
+++ Bluetooth_Module.ino
@@ -0,0 +1,42 @@
+void setup() 
+{
+Serial.begin(9600);         
+pinMode(11, OUTPUT);
+pinMode(9,OUTPUT);         
+}
+void loop()
+{
+if(Serial.available() > 0)  
+{char data;
+Serial.print("\nbluetooth connected\n");
+data = Serial.read();
+ Serial.print("BLUETOOTH VALUE = ");
+ Serial.print(data);        
+ Serial.print("\n");             
+      
+if(data == '1') 
+{
+digitalWrite(11, HIGH);
+Serial.print("LOAD 1 ON\n"); 
+}
+if(data == '0')  
+{digitalWrite(11, LOW);
+Serial.print("LOAD 1 OFF\n"); 
+}
+if(data =='2')
+{
+digitalWrite(9,HIGH);
+Serial.print("LOAD 2 ON\n"); 
+}
+if(data =='9')
+{
+digitalWrite(9,LOW);
+Serial.print("LOAD 2 OFF\n"); 
+}  
+if(data =='5')
+{
+digitalWrite(9,LOW);
+Serial.print("LOAD 1 OFF\n LOAD 2 OFF"); 
+}                            
+}
+}
